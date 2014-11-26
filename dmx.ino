074b3dadd682df3b99f8aa1dcbee19d4aef82fa4
Nicholas Petty <nick@ihackeverything.com>
2014-11-26 11:02:25 -0600

    Pass in list instead of sequence

--- dmx.ino
+++ dmx.ino
@@ -3,32 +3,63 @@
 int channels[511];
 
 void setup() {
-	Serial.begin(115200);
-	for (int i = 0; i < 511; i++) {
-		channels[i] = 0;
-	}
+  Serial.begin(115200);
+  for (int i = 0; i < 511; i++) {
+    channels[i] = 0;
+  }
   DmxMaster.write(channels);
 }
 
-int value = 0;
+
 int channel;
+int data;
+int value = 0;
+int i = 0;  
+// void loop() {
+//   int c;
+
+//   while(!Serial.available());
+//   c = Serial.read();
+//   if ((c>='0') && (c<='9')) {
+//     value = 10*value + c - '0';
+//   } else {
+//     if (c=='c') channel = value;
+//     else if (c=='w') {
+//       channels[channel] = value;
+//       int i;
+//       // for (i=0; i < 5; i++){
+//       //   Serial.print(channels[i]);
+//       // }
+//       DmxMaster.write(channels);
+//       Serial.println();
+//     }
+//     value = 0;
+//     }
+// }
 
 void loop() {
   int c;
 
-  while(!Serial.available());
-  c = Serial.read();
-  if ((c>='0') && (c<='9')) {
-    value = 10*value + c - '0';
-  } else {
-    if (c=='c') channel = value;
-    else if (c=='w') {
-      channels[channel] = value;
+
+  while(!Serial.available());//{
+
+    c = Serial.read();
+
+    if ((c>='0') && (c<='9')){
+      value = 10*value + c - '0';
+    }
+
+    if (c ==',') {
+      channels[i] = value;
+      i++;
+      value = 0;
+    }
+
+    if (c=='~') {
+      channels[i] = value;
       DmxMaster.write(channels);
-      Serial.println();
+      i = 0;
+      value=0; 
     }
-    value = 0;
-    }
+  //}
 }
-
-
